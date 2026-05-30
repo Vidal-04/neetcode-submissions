118ccf7816d9223115b4dd160426020620dd333e
Joshua <130109783+Vidal-04@users.noreply.github.com>
2026-05-29 22:03:42 -0300

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        res = right
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            total_time = sum(math.ceil(pile/mid) for pile in piles)
+            
+            if total_time <= h:
+                res = mid
+                right = mid - 1
+
+            else:
+                left = mid + 1
+
+        return res
+
+        
